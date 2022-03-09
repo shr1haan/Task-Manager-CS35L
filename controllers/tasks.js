e948f8f2252c8e4ebfbeef6270cbef6280ac5693
Shrihaan Gangopadhyay <shrihaang@gmail.com>
2022-03-08 23:59:55 -0800

    Added Basic Routes * route for getting all tasks * route for creating new task * route for deleting task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,9 +1,25 @@
 
+// controller for fetching all tasks
 const get_all_tasks = (req, res) => {
-    res.send('all items from the file')
+    res.send('get all tasks')
+}
+
+// controller for creating new tasks
+const create_task = (req, res) => {
+    res.send('create new task')
+}
+
+// controller for getting info on one task
+const get_one_task = (req, res) => {
+    res.send('info on one task')
+}
+
+// controller for deleting the task
+const delete_task = (req, res) => {
+    res.send('delete task')
 }
 
 module.exports =
 {
-    get_all_tasks
+    get_all_tasks, create_task, get_one_task, delete_task
 }
