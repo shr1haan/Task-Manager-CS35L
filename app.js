316a07f7a17d59c5a397d6d771c270506ad4f061
Shrihaan Gangopadhyay <shrihaang@gmail.com>
2022-03-09 01:39:24 -0800

    Setup dotenv to hide MongoDB Connection String

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-require('./db/connect')
 const express = require('express')
 const app = express();
 const tasks = require('./routes/tasks')
 const port = 3000
+require('dotenv').config()
+
+const connect_to_db = require('./db/connect')
+
 
 //middleware info
 app.use(express.json())
@@ -14,5 +17,17 @@ app.get('/hello', (req, res) => {
 
 app.use('/api/v1/tasks', tasks)
 
+const start = async () => {
+    try {
+        // first connect to the databse
+        await connect_to_db(process.env.MONGO_KEY)
+        // if successful then listen on port
+        app.listen(port, console.log(`server is listening on port ${port}...`))
 
-app.listen(port, console.log(`server is listening on port ${port}...`))
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
+
+start()
--- db/connect.js
+++ db/connect.js
@@ -1,17 +1,16 @@
 // setup the database
 const mongoose = require('mongoose')
 
-//connection string from mongoDB
-const connection_string = 'mongodb+srv://Shrihaan:12345@task-manager.qumhg.mongodb.net/Task-Manager-DB?retryWrites=true&w=majority'
-
-mongoose
+const connect_to_db = (url) => {
+    return mongoose
     //connect to the database
-    .connect(connection_string, {
+    .connect(url, {
+        // added these to remove deprecation warnings in console
+        // basically lifted this from a tutorial
         useNewUrlParser: true,
         useCreateIndex: true,
         useFindAndModify: false,
         useUnifiedTopology: true})
-    //callback so we know if we connected
-    .then(()=>console.log('connected to db...'))
-    //if errors, then print to console
-    .catch((err)=>console.log(err))
+}
+
+module.exports = connect_to_db
